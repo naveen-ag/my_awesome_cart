c8472ec8668203bb1006c2457f9d45607978757b
naveen <naveen.equitypandit@gmail.com>
2025-06-06 17:34:44 +0530

    added product Model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    product_desc = models.CharField(max_length=300)
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return self.product_name
+
